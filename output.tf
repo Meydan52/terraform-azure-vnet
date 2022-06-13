43d20ee5bc4d77f741185dd771fd6817e578ca0f
Nizami <41498763+Meydan52@users.noreply.github.com>
2022-06-13 14:15:27 -0400

    output update

--- output.tf
+++ output.tf
@@ -2,17 +2,11 @@ output "vnet_id" {
   value = azurerm_virtual_network.vnet.id
 }
 
-
-output "subnet" {
-  value = azurerm_virtual_network.vnet.subnet
-}
-output "subnet_id" {
-  value = azurerm_virtual_network.vnet.id
-}
-
 output "vnet_name" {
   value = azurerm_virtual_network.vnet.name
 }
+
+
 output "resource_group_name" {
   value = azurerm_resource_group.vnet_rg.name
 }
@@ -21,6 +15,12 @@ output "resource_group_location" {
   value = azurerm_resource_group.vnet_rg.location
 }
 
+output "subnet_name" {
+  value = azurerm_virtual_network.vnet.subnet
+}
+output "subnet_id" {
+  value = azurerm_virtual_network.vnet.subnet.id
+}
 
 # output "full_info" {
 #  value = data.terraform_remote_state.main.outputs.*
